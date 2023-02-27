0a104c6a1afc82ec36f89778581fffb9905bf1cc
Hengxian Jiang <Stanleyyjhx@gmail.com>
2023-02-27 20:03:40 +0800

    create default layout; add navbar.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import { FileOutlined, PieChartOutlined, UserOutlined, DesktopOutlined, TeamOutlined, HomeOutlined} from '@ant-design/icons';
+import { Breadcrumb, Layout, Menu, theme } from 'antd';
+import { useState } from 'react';
+import { useNavigate, Routes, Route } from 'react-router-dom'
+import Home from './components/Home/Home';
+import './App.css'
+import Navbar from './components/Header/Header';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const { Header, Content, Footer, Sider } = Layout;
+function getItem(label, key, icon, children) {
+  return {
+    key,
+    icon,
+    children,
+    label,
+  };
 }
+const items = [
+  getItem('Home', '/home', <HomeOutlined />),
+  getItem('Profile', '/profile', <UserOutlined />),
+  getItem('Group', '/group', <TeamOutlined />),
+  getItem('Group Management', '/group_mgmt', <DesktopOutlined />),
+];
+const App = () => {
+  const [collapsed, setCollapsed] = useState(false);
+  const navigate = useNavigate();
 
+  const onClick = (e) => {
+    navigate(e.key, {replace: true});
+  }
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
+  return (
+    <Layout
+      style={{
+        minHeight: '100vh',
+      }}
+    >
+      <Sider className='sidebar' collapsible collapsed={collapsed} onCollapse={(value) => setCollapsed(value)}>
+        <div className='sidebar__title' >
+        </div>
+        <Menu theme="dark" defaultSelectedKeys={['/home']} mode="inline" items={items} onClick={onClick}/>
+      </Sider>
+      <Layout className="site-layout">
+          <Navbar />
+        <Content
+          style={{
+            margin: '0 16px',
+          }}
+        >
+          <Breadcrumb
+            style={{
+              margin: '16px 0',
+            }}
+          >
+            <Breadcrumb.Item>User</Breadcrumb.Item>
+            <Breadcrumb.Item>Bill</Breadcrumb.Item>
+          </Breadcrumb>
+          <div
+            className='page__content'
+            style={{
+              padding: 24,
+              minHeight: 560,
+              background: colorBgContainer,
+            }}
+          >
+            <Routes>
+              <Route exact path='/home' element={<Home />}></Route>
+            </Routes>
+          </div>
+        </Content>
+        <Footer
+          style={{
+            textAlign: 'center',
+          }}
+        >
+          Study Buddy Platform ©2023
+        </Footer>
+      </Layout>
+    </Layout>
+  );
+};
 export default App;
---
+++ frontend/src/components/Home/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const Home = () => {
+    return (
+        <div>This is home page</div>
+    )
+}
+
+export default Home;
